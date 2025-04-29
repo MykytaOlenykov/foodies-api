5cc850aed6b0ed2cceb3e8981601deb1e2ccba39
Mykyta <nikita2001395@gmail.com>
2025-04-30 00:56:20 +0300

    remove avatar resize

--- services/filesServices.js
+++ services/filesServices.js
@@ -4,45 +4,41 @@ import path from "node:path";
 import sharp from "sharp";
 
 const fileCategories = {
-    avatars: "avatars",
+  avatars: "avatars",
 };
 
 const staticDir = path.resolve("public");
 
 const removeFile = async (filePath, isFullPath) => {
-    try {
-        const fullPath = isFullPath ? filePath : path.join(staticDir, filePath);
-        await fs.access(fullPath);
-        await fs.unlink(fullPath);
-    } catch (error) {
-        if (error.code !== "ENOENT") {
-            console.log(`Error removing file: ${error.message}`);
-        }
+  try {
+    const fullPath = isFullPath ? filePath : path.join(staticDir, filePath);
+    await fs.access(fullPath);
+    await fs.unlink(fullPath);
+  } catch (error) {
+    if (error.code !== "ENOENT") {
+      console.log(`Error removing file: ${error.message}`);
     }
+  }
 };
 
 const processAvatar = async (file) => {
-    const { filename, path: tmpUpload } = file;
-    const fileFormat = path.extname(filename).toLowerCase().slice(1);
+  const { filename, path: tmpUpload } = file;
+  const fileFormat = path.extname(filename).toLowerCase().slice(1);
 
-    const avatarsDir = path.join(staticDir, fileCategories.avatars);
-    const resultUpload = path.join(avatarsDir, filename);
+  const avatarsDir = path.join(staticDir, fileCategories.avatars);
+  const resultUpload = path.join(avatarsDir, filename);
 
-    await sharp(tmpUpload)
-        .resize(240, 240, {
-            fit: sharp.fit.cover,
-            position: sharp.strategy.entropy,
-        })
-        .toFormat(fileFormat, { quality: 80 })
-        .toFile(resultUpload);
+  await sharp(tmpUpload)
+    .toFormat(fileFormat, { quality: 80 })
+    .toFile(resultUpload);
 
-    await removeFile(tmpUpload, true);
-    const avatarURL = path.join(path.sep, fileCategories.avatars, filename);
+  await removeFile(tmpUpload, true);
+  const avatarURL = path.join(path.sep, fileCategories.avatars, filename);
 
-    return avatarURL;
+  return avatarURL;
 };
 
 export const filesServices = {
-    removeFile,
-    processAvatar,
+  removeFile,
+  processAvatar,
 };
