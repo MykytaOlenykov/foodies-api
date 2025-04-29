00a3271cb3461803678453d3740e23de81ad416f
ArthurMykhailiuk <artur.myhajlyuk@volo.global>
2025-04-29 20:46:43 +0300

    add validation for name

--- schemas/authSchemas.js
+++ schemas/authSchemas.js
@@ -2,6 +2,7 @@ import Joi from "joi";
 import { emailRegexp } from "../constants/auth.js";
 
 export const registerSchema = Joi.object({
+  name: Joi.string().min(2).max(30).required(),
   email: Joi.string().pattern(emailRegexp).min(4).max(255).required(),
   password: Joi.string().min(6).max(255).required(),
 });
