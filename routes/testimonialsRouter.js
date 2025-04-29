801284891c0153a58b2d039c9738696f2e431446
Mykyta <nikita2001395@gmail.com>
2025-04-29 23:11:43 +0300

    refactor areas

--- routes/testimonialsRouter.js
+++ routes/testimonialsRouter.js
@@ -2,12 +2,23 @@ import express from "express";
 import { testimonialsControllers } from "../controllers/testimonialsControllers.js";
 import { authenticate } from "../middlewares/authenticate.js";
 import { validateBody } from "../middlewares/validateBody.js";
-import { createTestimonialSchema, getAllTestimonialsSchema } from "../schemas/testimonialSchema.js";
+import { createTestimonialSchema } from "../schemas/testimonialSchema.js";
+import { paginationSchema } from "../schemas/commonSchemas.js";
 import { validateQueryString } from "../middlewares/validateQueryString.js";
 
 const testimonialsRouter = express.Router();
 
-testimonialsRouter.get("/", validateQueryString(getAllTestimonialsSchema), testimonialsControllers.getAllTestimonials);
-testimonialsRouter.post("/",authenticate, validateBody(createTestimonialSchema), testimonialsControllers.createTestimonial);
+testimonialsRouter.get(
+  "/",
+  validateQueryString(paginationSchema),
+  testimonialsControllers.getAllTestimonials
+);
+
+testimonialsRouter.post(
+  "/",
+  authenticate,
+  validateBody(createTestimonialSchema),
+  testimonialsControllers.createTestimonial
+);
 
 export { testimonialsRouter };
