5ee09e15888e8ec1385f728d95db9f9f31115451
Ruslan <ruslan.grudistov@gmail.com>
2025-04-29 18:32:55 +0300

    schema

--- routes/recipesRouter.js
+++ routes/recipesRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-
+import { querySchema } from "../schemas/validateQueryString.js";
 import { recipesController } from "../controllers/recipesControllers.js";
+import { validateQueryString } from "../middlewares/validateQueryString.js";
 
 export const recipesRouter = express.Router();
 
-recipesRouter.get("/", recipesController.getFilteredRecipesController);
+recipesRouter.get("/", validateQueryString(querySchema), recipesController.getFilteredRecipesController);
 
 recipesRouter.get("/popular", recipesController.getPopularRecipesController);
 
