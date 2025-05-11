40dadc4cb411bbd443a2b1b62f61bc6d661392ab
Mykyta <nikita2001395@gmail.com>
2025-05-11 16:56:44 +0300

    fix file size validation

--- middlewares/imageUpload.js
+++ middlewares/imageUpload.js
@@ -34,5 +34,5 @@ const multerConfig = multer.diskStorage({
 export const imageUpload = multer({
   storage: multerConfig,
   fileFilter,
-  limits: { fileSize: 3 * 1024 * 1024 },
+  limits: { fileSize: 15 * 1024 * 1024 },
 });
